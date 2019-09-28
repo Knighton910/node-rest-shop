ac42ca4d8341899cd9642f528a47a227563b1a09
Kelvin Kni <keltheceo@gmail.com>
2019-09-27 17:25:17 -0700

    added auth: email and delete route

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,33 +6,54 @@ const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 router.post('/signup', (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).json({
-                error: err
-            });
-        }else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
-            })
-            user
-                .save()
-                .then(result => {
-                    console.log(result)
-                    res.status(201).json({
-                        message: 'User created'
-                    })
-                })
-                .catch(err => {
-                    console.log(err)
-                    res.status(500).json({
-                        error: err
-                    })
+    User.find({email: req.body.email})
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'Email exists already'
                 });
-        }
-    })
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    }else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        })
+                        user
+                            .save()
+                            .then(result => {
+                                console.log(result)
+                                res.status(201).json({
+                                    message: 'User created'
+                                })
+                            })
+                            .catch(err => {
+                                console.log(err)
+                                res.status(500).json({
+                                    error: err
+                                })
+                            });
+                    }
+                })
+            }
+        });
+})
+
+router.delete('/:userId', (req, res, next) => {
+    User.remove({_id: req.params.userId})
+        .exec()
+        .then(response => {
+            response.status(200).json({
+                message: 'User deleted'
+            });
+        })
+        .catch(err => res.status(500).json({error: err}));
 })
 
 module.exports = router;
